38153ed40c91d0b9d757f2f162d3b62929b2bf18
Victor Grigoriu <vgrigoriu@gmail.com>
2019-02-23 09:32:46 +0200

    Small perf improvements

--- atbash-cipher/atbash_cipher.go
+++ atbash-cipher/atbash_cipher.go
@@ -8,38 +8,32 @@ import (
 // Atbash returns the Atbash encryption of a plain text.
 func Atbash(plain string) string {
 	var result strings.Builder
-	cipherLength := 0
 	for _, r := range plain {
 		ok, sr := substitute(r)
 		if !ok {
 			// skip non-letters and non-digits
 			continue
 		}
-		if shouldAddSeparator(cipherLength) {
+		if result.Len()%6 == 5 {
 			result.WriteRune(' ')
 		}
 		result.WriteRune(sr)
-		cipherLength++
 	}
 
 	return result.String()
 }
 
-const alphabet string = "abcdefghijklmnopqrstuvwxyz"
-
 func substitute(r rune) (bool, rune) {
 	if '0' <= r && r <= '9' {
 		return true, r
 	}
-	var index int
 	if 'a' <= r && r <= 'z' {
-		index = int(r - 'a')
-	} else if 'A' <= r && r <= 'Z' {
-		index = int(r - 'A')
-	} else {
-		return false, 0
+		return true, 'a' + 'z' - r
 	}
-	return true, rune(alphabet[len(alphabet)-index-1])
+	if 'A' <= r && r <= 'Z' {
+		return true, 'a' + 'Z' - r
+	}
+	return false, 0
 }
 
 func shouldAddSeparator(length int) bool {
