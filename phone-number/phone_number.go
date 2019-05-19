e373035cac2408a69fe9f18d269d21995e3cc62b
Victor Grigoriu <vgrigoriu@gmail.com>
2019-05-19 19:18:47 -0400

    Simplify Phone Number solution

--- phone-number/phone_number.go
+++ phone-number/phone_number.go
@@ -3,13 +3,19 @@ package phonenumber
 
 import (
 	"fmt"
-	"strings"
-	"unicode"
+	"regexp"
 )
+
+// phoneNumber represents a NANP phone number.
+type phoneNumber struct {
+	areaCode     string
+	exchangeCode string
+	subscriber   string
+}
 
 // Number cleans up a valid NANP number by removing punctuation and the optional country code.
 func Number(input string) (number string, err error) {
-	n, err := phoneNumber(input)
+	n, err := parse(input)
 	if err != nil {
 		return "", err
 	}
@@ -19,7 +25,7 @@ func Number(input string) (number string, err error) {
 
 // AreaCode returns the area code of a valid NANP phone number.
 func AreaCode(input string) (number string, err error) {
-	n, err := phoneNumber(input)
+	n, err := parse(input)
 	if err != nil {
 		return "", err
 	}
@@ -29,7 +35,7 @@ func AreaCode(input string) (number string, err error) {
 
 // Format formats a valid NANP phone number.
 func Format(input string) (number string, err error) {
-	n, err := phoneNumber(input)
+	n, err := parse(input)
 	if err != nil {
 		return "", err
 	}
@@ -37,86 +43,21 @@ func Format(input string) (number string, err error) {
 	return n.format(), nil
 }
 
-type PhoneNumber struct {
-	areaCode     string
-	exchangeCode string
-	subscriber   string
+func parse(input string) (phoneNumber, error) {
+	//                            (223) 456-7890
+	re := regexp.MustCompile(`^[+1 (]*([2-9]\d\d)[) .-]*([2-9]\d\d)[ .-]*(\d{4})\s*$`)
+	matches := re.FindStringSubmatch(input)
+	if len(matches) == 0 {
+		return phoneNumber{}, fmt.Errorf("invalid phone number")
+	}
+
+	return phoneNumber{matches[1], matches[2], matches[3]}, nil
 }
 
-type parserState int
-
-const (
-	beforeAreaCode parserState = iota
-	inAreaCode
-	beforeExchangeCode
-	inExchangeCode
-	inSubscriber
-	done
-)
-
-func phoneNumber(input string) (PhoneNumber, error) {
-	state := beforeAreaCode
-	var areaCode, exchangeCode, subscriber strings.Builder
-	for _, r := range input {
-		// skip any punctuation
-		if !unicode.IsDigit(r) {
-			continue
-		}
-		switch state {
-		case beforeAreaCode:
-			// first digit must be either 1 (country code) or 2-9 (first digit of area code)
-			if r == '0' {
-				return PhoneNumber{}, fmt.Errorf("unexpected first digit %q", r)
-			}
-			if r == '1' {
-				// skip optional leading country code
-				break
-			}
-			// we have a valid first area code digit
-			areaCode.WriteRune(r)
-			state = inAreaCode
-		case inAreaCode:
-			// last two digits can be any digit
-			areaCode.WriteRune(r)
-			if areaCode.Len() == 3 {
-				// we have enough digits for the area code
-				state = beforeExchangeCode
-			}
-		case beforeExchangeCode:
-			// first digit of exchange code must be 2-9
-			if r == '0' || r == '1' {
-				return PhoneNumber{}, fmt.Errorf("unexpected first digit of exchange code %q", r)
-			}
-			exchangeCode.WriteRune(r)
-			state = inExchangeCode
-		case inExchangeCode:
-			// last two digit can be anything
-			exchangeCode.WriteRune(r)
-			if exchangeCode.Len() == 3 {
-				// we have enough digits for the exchange code
-				state = inSubscriber
-			}
-		case inSubscriber:
-			// any four digits
-			subscriber.WriteRune(r)
-			if subscriber.Len() == 4 {
-				state = done
-			}
-		case done:
-			// already got all the digits we need
-			return PhoneNumber{}, fmt.Errorf("too many digits")
-		}
-	}
-	if state != done {
-		return PhoneNumber{}, fmt.Errorf("not enough digits")
-	}
-	return PhoneNumber{areaCode.String(), exchangeCode.String(), subscriber.String()}, nil
-}
-
-func (n *PhoneNumber) number() string {
+func (n *phoneNumber) number() string {
 	return fmt.Sprintf("%s%s%s", n.areaCode, n.exchangeCode, n.subscriber)
 }
 
-func (n *PhoneNumber) format() string {
+func (n *phoneNumber) format() string {
 	return fmt.Sprintf("(%s) %s-%s", n.areaCode, n.exchangeCode, n.subscriber)
 }
